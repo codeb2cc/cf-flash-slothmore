3c9d8780956c692bcc31eb324e20e63fa9cb5e85
Codeb Fan <codeb2cc@gmail.com>
2020-01-02 15:14:23 +0800

    Add Normal/Erlang Distribution latency simulation.

--- README.md
+++ README.md
@@ -6,12 +6,21 @@ A Cloudflare Worker to simulator response lantency. Use it to test benchmark too
 
 Currently only GET request is supported. There're servel params to control how the simulator works:
 
-* `lmin`: Min lantency in ms to add to the response. Default: 10
-* `lmax`: Max lantency in ms to add to the response. Default: 20
-* `echo`: Response body if provided
-* `proxy`: Valid url to fetch and return to client. There's a `whitelist` to control which hosts can be proxied. The default whitelist is `['www.google.com', 'postb.in']`. Deploy your own worker if you want to proxy to more hosts.
+- `lmin`: Min lantency in ms to add to the response. Default: `10`
+- `lmax`: Max lantency in ms to add to the response. Default: `20`
+- `echo`: Response body if provided
+- `proxy`: Valid url to fetch and return to client. There's a `whitelist` to control which hosts can be proxied. The default whitelist is `['www.google.com', 'postb.in']`. Deploy your own worker if you want to proxy to more hosts.
+- `d`: Distribution method: `uniform` | `normal` | `erlang`. Default: `uniform`. For normal distribution, there're two optional params `d_mu`(mean, default to `(lmin + lmax) / 2`) and `d_sigma`(standard deviation, default to 1). For erlang distribution, `d_k`(shape, default to 1) and `d_mu`(scale, default to `(lmax - lmin) / 2`).
 
-For example, `curl "http://flash.codeb2cc.com/?lmin=100&lmax=200"` will respone in a random delay between [100, 200)ms. `curl "http://flash.codeb2cc.com/?proxy=https%3A//www.google.com/"` will fetch 'https://www.google.com/' and return in a [10, 20)ms delay.
+For example, `curl "http://flash.codeb2cc.com/?lmin=100&lmax=200"` will respone in a random delay between [100, 200)ms. `curl "http://flash.codeb2cc.com/?proxy=https%3A//www.google.com/"` will fetch 'https://www.google.com/' and return in a [10, 20)ms delay. More:
+
+```
+curl "http://flash.codeb2cc.com/?lmin=100&lmax=200&d=normal&d_sigma=0.5"
+
+curl "http://flash.codeb2cc.com/?lmin=100&lmax=200&d=normal&d_mu=130"
+
+curl "http://flash.codeb2cc.com/?lmin=0&lmax=100&d=erlang&d_k=2&d_mu=30"
+```
 
 
 ### 👩 💻 Developing
@@ -26,11 +35,11 @@ wrangler generate my-app https://github.com/codeb2cc/cf-flash-slothmore
 
 More features:
 
-* Different lantency distribution models (normal/poisson/erlang and more)
-* Support simulator params(`lmin`, `lmax`, etc) in request headers
-* Cache proxy response to protect target API service
-* Custom cache missing rate and ttl
-* Predefine response data read from Cloudflare KV
+- [x] Different lantency distribution models (normal/erlang(poisson) and more)
+- [ ] Support simulator params(`lmin`, `lmax`, etc) in request headers
+- [ ] Cache proxy response to protect target API service
+- [ ] Custom cache missing rate and ttl
+- [ ] Predefine response data read from Cloudflare KV
 
 
 ![flash-slothmore](flash-slothmore.jpg)
--- src/handler.ts
+++ src/handler.ts
@@ -7,10 +7,11 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
 
   let params = (new URL(request.url)).searchParams
-  let latencyMax = parsePositive(params.get('lmax'), 20)
-  let latencyMin = Math.min(parsePositive(params.get('lmin'), 10), latencyMax)
+  let latencyMax = parsePositiveInt(params.get('lmax'), 20)
+  let latencyMin = Math.min(parsePositiveInt(params.get('lmin'), 10), latencyMax)
   let echo = params.get('echo')
   let proxy = params.get('proxy')
+  let distributionFunc = params.get('d') || 'uniform'
 
   const hello = `Nice...to...see you...too!`
   let response = echo != undefined ? new Response(echo) : new Response(hello)
@@ -31,28 +32,89 @@ export async function handleRequest(request: Request): Promise<Response> {
   }
 
   if (latencyMax != 0) {
-    await delay(randInt(latencyMin, latencyMax))
+    let lantency = 0
+    try {
+      switch (distributionFunc) {
+        case 'uniform':
+          lantency = randUniform(latencyMin, latencyMax)
+          break
+        case 'normal': {
+          let mu = (latencyMin + latencyMax) / 2
+          let sigma = 1
+          if (params.get('d_mu') != undefined) {
+            mu = parsePositiveInt(params.get('d_mu'), mu)
+          }
+          if (params.get('d_sigma') != undefined) {
+            sigma = parsePositiveFloat(params.get('d_sigma'), sigma)
+          }
+          lantency = Math.max(Math.min(randNormal(mu, sigma), latencyMax), latencyMin)
+          break
+        }
+        case 'erlang': {
+          let k = 1 // Shape
+          if (params.get('d_k') != undefined) {
+            k = parsePositiveInt(params.get('d_k'), k)
+          }
+          let mu = (latencyMax - latencyMin) / 2  // Scale
+          if (params.get('d_mu') != undefined) {
+            mu = parsePositiveFloat(params.get('d_mu'), mu)
+          }
+          lantency = Math.min(randErlang(k, 1 / mu) + latencyMin, latencyMax)
+          break;
+        }
+        default:
+          throw new Error(`Unsupported distribution method ${distributionFunc}`)
+      }
+    } catch (error) {
+      return new Response(JSON.stringify({ error: error.message }), BadRequest)
+    }
+
+    await delay(lantency)
   }
   return response
 }
 
-function parsePositive(input: string | null, defaultVal?: number): number {
+function delay(ms: number): Promise<any> {
+  return new Promise(resolve => setTimeout(resolve, Math.ceil(ms)))
+}
+
+function parsePositiveInt(input: string | null, defaultVal?: number): number {
   let value = parseInt(input || 'NaN')
-  if (value == NaN) {
+  if (value == NaN || value < 0) {
     value = defaultVal || 0
-  }
-  if (value < 0) {
-    value = 0
   }
   return value
 }
 
-function delay(ms: number): Promise<any> {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function parsePositiveFloat(input: string | null, defaultVal?: number): number {
+  let value = parseFloat(input || 'NaN')
+  if (value == NaN || value < 0) {
+    value = defaultVal || 0
+  }
+  return value
 }
 
-function randInt(min: number, max: number): number {
+function randUniform(min: number, max: number): number {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min)) + min
+}
+
+function randNormal(mu: number, sigma: number): number {
+  let x, y, r
+  do {
+    x = Math.random() * 2 - 1
+    y = Math.random() * 2 - 1
+    r = x * x + y * y
+  } while (!r || r > 1)
+  return mu + sigma * y * Math.sqrt(-2 * Math.log(r) / r)
+}
+
+// https://en.wikipedia.org/wiki/Erlang_distribution#Generating_Erlang-distributed_random_variates
+function randErlang(k: number, lambda: number): number {
+  let u = 1
+  for (let i = 0; i < k; i++) {
+    u = u * Math.random()
+  }
+  return - (1 / lambda) * Math.log(u)
 }
